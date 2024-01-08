7833e32f640aa4b20efe6201502b1d0157860b30
llcain <llcain2012@gmail.com>
2024-01-07 23:45:09 -0600

    Updated index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const path = require('path');
+const adminRoute = require('./routes/admin');
+const exp = require('constants');
 
 const app = express();
+
+// EJS template engine setup
+app.set('view engine', 'ejs');
+app.set('views', './src/pages');
+
+// Setting up the directory on the server for CSS, JavaScript and media files
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
+// Configuring the server to work with fomr submissions and json files
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Connecting all of the routes
+app.use('/', adminRoute);
 
 app.get('/', (req, res) => res.send('Home Route'));
 
